3b21b009a189a829e8bba07b2a570ac45226f14c
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-12-14 13:23:46 +0100

    Add condition to characterDetail component

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -5,33 +5,41 @@ const CharacterDetail = (props) => {
   const params = useParams();
   const characterSelected = props.findCharacter(params.id);
 
-  return (
-    <section className='details'>
-      <h1 className='details__title'>Character Details</h1>
-      <li className='details__list'>
-        <article className='details__article'>
-          <h2 className='details__name'>{characterSelected.name}</h2>
-          <div className='details__container'>
-            <div className='main__content-image-container'>
-              <img
-                className='main__content-image-characters'
-                src={characterSelected.image}
-                alt={characterSelected.name}
-                title={`Photo of ${characterSelected.name}`}
-              />
-            </div>
-            <p className='details__sub'>Species: {characterSelected.species}</p>
-            <p className='details__sub'>Origin: {characterSelected.origin}</p>
-            <p className='details__sub'>Status: {characterSelected.status}</p>
-            <p className='details__sub'>Appears in {characterSelected.episode} episodes</p>
-          </div>
-        </article>
-        <Link className='details__link' to='/'>
-          Go back
-        </Link>
-      </li>
-    </section>
-  );
+  const renderDetails = () => {
+    if (characterSelected !== undefined) {
+      return (
+        <section className='details'>
+          <h1 className='details__title'>Character Details</h1>
+          <li className='details__list'>
+            <article className='details__article'>
+              <h2 className='details__name'>{characterSelected.name}</h2>
+              <div className='details__container'>
+                <div className='main__content-image-container'>
+                  <img
+                    className='main__content-image-characters'
+                    src={characterSelected.image}
+                    alt={characterSelected.name}
+                    title={`Photo of ${characterSelected.name}`}
+                  />
+                </div>
+                <p className='details__sub'>Species: {characterSelected.species}</p>
+                <p className='details__sub'>Origin: {characterSelected.origin}</p>
+                <p className='details__sub'>Status: {characterSelected.status}</p>
+                <p className='details__sub'>Appears in {characterSelected.episode} episodes</p>
+              </div>
+            </article>
+            <Link className='details__link' to='/'>
+              Go back
+            </Link>
+          </li>
+        </section>
+      );
+    } else {
+      <h2 className='not__found'>Page not found</h2>;
+    }
+  };
+
+  return <>{renderDetails()}</>;
 };
 
 export { CharacterDetail };
